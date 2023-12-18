eab1b6c06f4de01c5473ea167adfe0edb5e70183
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2023-12-18 13:17:18 +0300

    feat: add carbon dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,25 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (!validateInput(sampleActivity)) return false;
+
+  function validateInput(input) {
+    if (!sampleActivity) return false;
+    if (typeof input !== "string") return false;
+    if (isNaN(input)) return false;
+    if (!isFinite(input)) return false;
+
+    return true;
+  }
+
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
+
+
+  if (!isFinite(t) || t < 0) return false;
+
+  return t;
 }
 
 module.exports = {
