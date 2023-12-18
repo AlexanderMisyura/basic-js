a97ccebab924a2def5f665ae761ea94215471825
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2023-12-18 14:04:02 +0300

    feat: add common char count and count cats solutions

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let charMap = {};
+  let s2Copy = s2;
+
+  s1.replace(/./g, (char) => {
+    if (s2Copy.includes(char)) {
+      charMap[char] = charMap[char] + 1 || 1;
+      s2Copy = s2Copy.replace(char, "");
+    }
+  });
+
+  const commonChars = Object.values(charMap).reduce(
+    (acc, curr) => acc + curr,
+    0
+  );
+
+  return commonChars;
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(backyard) {
+  const cat = "^^";
+  let catCounter = 0;
+
+  for (const someYardThing of backyard) {
+    for (const catOrNot of someYardThing) {
+      if (catOrNot === cat) catCounter++;
+    }
+  }
+
+  return catCounter;
 }
 
 module.exports = {
