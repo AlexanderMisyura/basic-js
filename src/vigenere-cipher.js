29f090dadb186b8a7fc953e93e9edb0281489511
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2023-12-20 00:40:09 +0300

    feat: add vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,86 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    const firstKeyCode = "A".charCodeAt(0);
+    const lastKeyCode = "Z".charCodeAt(0);
+    const messageUpper = message.toUpperCase();
+    let keyString = "";
+    let encrypted = "";
+
+    for (let i = 0, j = 0; i < messageUpper.length; i++, j++) {
+      if (j > key.length - 1) j = 0;
+      const charCode = messageUpper.charCodeAt(i);
+      if (charCode < firstKeyCode || charCode > lastKeyCode) {
+        keyString += messageUpper[i];
+        j--;
+      } else {
+        keyString += key[j].toUpperCase();
+      }
+    }
+
+    for (let i = 0; i < keyString.length; i++) {
+      const currentCharCode = messageUpper.charCodeAt(i);
+      if (currentCharCode < firstKeyCode || currentCharCode > lastKeyCode) {
+        encrypted += messageUpper[i];
+      } else {
+        const currentKeyCode = keyString.charCodeAt(i);
+        const shift = currentKeyCode - firstKeyCode;
+        const encriptedCharCode =
+          ((currentCharCode - firstKeyCode + shift) % 26) + firstKeyCode;
+        encrypted += String.fromCharCode(encriptedCharCode);
+      }
+    }
+
+    return this.direct ? encrypted : encrypted.split("").reverse().join("");
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    const firstKeyCode = "A".charCodeAt(0);
+    const lastKeyCode = "Z".charCodeAt(0);
+    const messageUpper = encryptedMessage.toUpperCase();
+    let keyString = "";
+    let decrypted = "";
+
+    for (let i = 0, j = 0; i < messageUpper.length; i++, j++) {
+      if (j > key.length - 1) j = 0;
+      const charCode = messageUpper.charCodeAt(i);
+      if (charCode < firstKeyCode || charCode > lastKeyCode) {
+        keyString += messageUpper[i];
+        j--;
+      } else {
+        keyString += key[j].toUpperCase();
+      }
+    }
+
+    for (let i = 0; i < keyString.length; i++) {
+      const currentCharCode = messageUpper.charCodeAt(i);
+      if (currentCharCode < firstKeyCode || currentCharCode > lastKeyCode) {
+        decrypted += messageUpper[i];
+      } else {
+        console.log(keyString[i]);
+        const currentKeyCode = keyString.charCodeAt(i);
+        const shift = currentKeyCode - firstKeyCode;
+        console.log(currentCharCode);
+        console.log(shift);
+        const encriptedCharCode =
+          ((currentCharCode - firstKeyCode - shift + 26) % 26) + firstKeyCode;
+        console.log(String.fromCharCode(encriptedCharCode));
+        decrypted += String.fromCharCode(encriptedCharCode);
+      }
+    }
+
+    return this.direct ? decrypted : decrypted.split("").reverse().join("");
   }
 }
 
